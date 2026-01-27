8790a26990a7e2b35adae9b29e2421de95745966
Muhammad-Bilal1605 <zaheermbilal1605@gmail.com>
2026-01-27 14:10:25 +0500

    Initial commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
+
+@Controller('auth')
+export class AuthController {
+    constructor(private authService : AuthService) {}
+
+    @Post('signup')
+    signup(@Body() dto:AuthDto){
+        console.log('DTO received:', dto);
+        return this.authService.signUp(dto)
+    }
+
+    @Post('signin')
+    signin(){
+        return this.authService.signin
+    }
+}
